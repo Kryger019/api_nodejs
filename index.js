5cadbc8204381ce28313fcbf66ad84c91b254667
Kryger019 <160683097+Kryger019@users.noreply.github.com>
2024-03-14 21:21:02 -0300

    api_nodejs

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
-
-const app = express ();
+const cors = require('cors');
 
 //const porta = process.env.PORT || 3333;
-const porta = 3333;
+const router = require('./routes/routes');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
 
 app.listen(porta, () => {
     console.log('Servidor iniciado na porta' + porta);
